624effdb005f5f75e6bb995d16592c2d9818f13d
albertsparrman <77719327+albertsparrman@users.noreply.github.com>
2022-02-07 11:12:05 +0100

    Försökte göra om string till en lista

--- src/perolof.java
+++ src/perolof.java
@@ -1,2 +1,24 @@
-public class perolof {
+import javax.swing.*;
+import java.util.*;
+
+class Ord {
+    public static List<Character>
+    convertStringToCharList(String str) {
+        List<Character> chars = new ArrayList<>();
+        for (char ch : str.toCharArray()){
+            chars.add(ch);
+        }
+        return chars;
+    }
 }
+
+public class perolof {
+    public static void main(String[] args){
+        String str = JOptionPane.showInputDialog("Välj ett episkt ord");
+
+        List<Character>
+                chars = convertStringToCharList(str);
+        System.out.println(chars);
+    }
+}
+
